edb8585161cc6d0ef334de0be9cfe903e74536a7
Jake Day <jake@ninebysix.com>
2012-01-30 20:15:42 -0500

    fixed SGX cache operation by using a single ioctl in kernel side, required for oc'd gpu

--- drivers/gpu/pvr/services.h
+++ drivers/gpu/pvr/services.h
@@ -426,6 +426,13 @@ typedef struct _PVRSRV_MISC_INFO_
 
 		
 		IMG_UINT32	ui32Length;
+
+		IMG_BYTE *pbRowStart;
+		IMG_BYTE *pbRowEnd;
+		IMG_BYTE *pbRowThresh;
+		IMG_BOOL bStridedCacheOp;
+		IMG_UINT32 ui32Stride;
+
 	} sCacheOpCtl;
 
 	
