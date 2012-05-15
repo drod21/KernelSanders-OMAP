0a24bb6dc17a53e0127a39c41e5998800c2cf0c8
Derek Rodriguez <drod2169@gmail.com>
2012-05-15 09:05:06 -0400

    Revert "cpuidle: add support for states that affect multiple cpus"
    This reverts commit 9e28bf08edbf6f74ffb8f9c7677a14255439a81e.

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -48,7 +48,6 @@ struct cpuidle_state {
 /* Idle State Flags */
 #define CPUIDLE_FLAG_TIME_VALID	(0x01) /* is residency time measurable? */
 #define CPUIDLE_FLAG_IGNORE	(0x100) /* ignore during this idle period */
-#define CPUIDLE_FLAG_COUPLED	(0x02) /* state applies to multiple cpus */
 
 #define CPUIDLE_DRIVER_FLAGS_MASK (0xFFFF0000)
 
@@ -96,11 +95,7 @@ struct cpuidle_device {
 	void			*governor_data;
 	struct cpuidle_state	*safe_state;
 
-#ifdef CONFIG_ARCH_NEEDS_CPU_IDLE_COUPLED
-	int			safe_state_index;
-	cpumask_t		coupled_cpus;
-	struct cpuidle_coupled	*coupled;
-#endif
+	int (*prepare)		(struct cpuidle_device *dev);
 };
 
 DECLARE_PER_CPU(struct cpuidle_device *, cpuidle_devices);
