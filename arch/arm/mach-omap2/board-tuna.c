975d172a3a4ea135290bfc4aab15763761bfddce
Derek Rodriguez <drod2169@gmail.com>
2012-06-07 09:40:48 -0400

    Raise VUSIM and VAUX3 voltages for staility

--- arch/arm/mach-omap2/board-tuna.c
+++ arch/arm/mach-omap2/board-tuna.c
@@ -529,8 +529,8 @@ static struct regulator_consumer_supply tuna_vaux3_supplies[] = {
 
 static struct regulator_init_data tuna_vaux3 = {
 	.constraints = {
-		.min_uV			= 2750000,
-		.max_uV			= 2750000,
+		.min_uV			= 2900000,
+		.max_uV			= 2900000,
 		.apply_uV		= true,
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL
 					| REGULATOR_MODE_STANDBY,
@@ -586,8 +586,8 @@ static struct regulator_consumer_supply tuna_vusim_supplies[] = {
 
 static struct regulator_init_data tuna_vusim = {
 	.constraints = {
-		.min_uV			= 1750000,
-		.max_uV 		= 1750000,
+		.min_uV			= 1850000,
+		.max_uV 		= 1850000,
 		.apply_uV		= true,
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL
 					| REGULATOR_MODE_STANDBY,
