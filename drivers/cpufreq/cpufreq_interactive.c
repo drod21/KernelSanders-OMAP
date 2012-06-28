309e22b9c1e97c90d46dd21be651804924e57aee
Derek Rodriguez <drod2169@gmail.com>
2012-06-28 12:29:31 -0700

    Change tunables

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -87,13 +87,13 @@ static enum tune_values {
        HIGH_PERF_TUNE
 } cur_tune_value;
 
-#define MIN_GO_HISPEED_LOAD 70
-#define DEFAULT_LOW_POWER_RATE 10
+#define MIN_GO_HISPEED_LOAD 80
+#define DEFAULT_LOW_POWER_RATE 20
 
 /* default number of sampling periods to average before hotplug-in decision */
-#define DEFAULT_SAMPLING_PERIODS 10
+#define DEFAULT_SAMPLING_PERIODS 15
 #define DEFAULT_HI_PERF_THRESHOLD 80
-#define DEFAULT_LOW_POWER_THRESHOLD 35
+#define DEFAULT_LOW_POWER_THRESHOLD 45
 #define MAX_MIN_SAMPLE_TIME (80 * USEC_PER_MSEC)
 
 // used for suspend code
