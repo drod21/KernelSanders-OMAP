a993c8dcad99207b7f42ca7170ecf2b13e4bfe20
Suman Anna <s-anna@ti.com>
2012-07-03 19:13:59 -0500

    OMAP4: clocks: skip module idle status check for processor devices
    The module idle status for processor devices (hwmod devices) like IPU,
    DSP & IVA will fail the functional status check as long as the module
    is held in reset. The status would always show that the module is in
    transition, and would complete only after the relevant hwmod's reset
    line is deasserted.
    
    The initialization sequence for these processor devices require the
    clocks to be enabled first before deasserting the reset lines. This
    would cause a failure in _omap4_module_wait_ready which checks for
    the module idle status to be functional. The clk ops for dsp, ipu
    and iva in the clock data has therefore been redefined such that
    the module idlest status check in the clock enable code is skipped
    over.
    
    Change-Id: I799b30c5f86e51739a56538f602a8030fd4bdb09
    Signed-off-by: Oleg Matcovschi <oleg.matcovschi@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Juan Gutierrez <jgutierrez@ti.com>
    Signed-off-by: Paul Hunt <hunt@ti.com>
    
    Conflicts:
    
    	arch/arm/mach-omap2/clock44xx_data.c

--- arch/arm/mach-omap2/clock44xx_data.c
+++ arch/arm/mach-omap2/clock44xx_data.c
@@ -1505,7 +1505,7 @@ static struct clk dmic_fck = {
 
 static struct clk dsp_fck = {
 	.name		= "dsp_fck",
-	.ops		= &clkops_omap4_dflt_wait,
+	.ops		= &clkops_omap2_dflt,
 	.enable_reg	= OMAP4430_CM_TESLA_TESLA_CLKCTRL,
 	.enable_bit	= OMAP4430_MODULEMODE_HWCTRL,
 	.clkdm_name	= "tesla_clkdm",
@@ -1855,7 +1855,7 @@ static struct clk i2c4_fck = {
 
 static struct clk ipu_fck = {
 	.name		= "ipu_fck",
-	.ops		= &clkops_omap4_dflt_wait,
+	.ops		= &clkops_omap2_dflt,
 	.enable_reg	= OMAP4430_CM_DUCATI_DUCATI_CLKCTRL,
 	.enable_bit	= OMAP4430_MODULEMODE_HWCTRL,
 	.clkdm_name	= "ducati_clkdm",
@@ -1885,7 +1885,7 @@ static struct clk iss_fck = {
 
 static struct clk iva_fck = {
 	.name		= "iva_fck",
-	.ops		= &clkops_omap4_dflt_wait,
+	.ops		= &clkops_omap2_dflt,
 	.enable_reg	= OMAP4430_CM_IVAHD_IVAHD_CLKCTRL,
 	.enable_bit	= OMAP4430_MODULEMODE_HWCTRL,
 	.clkdm_name	= "ivahd_clkdm",
