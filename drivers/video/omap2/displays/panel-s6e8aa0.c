bd6b7f616e37fa7f3506af436289a1296c3e5c5e
Ezekeel <notezekeel@googlemail.com>
2012-07-06 23:56:10 +0200

    This is going to be reverted. Added Color Control version 4.
    Conflicts:
    
    	drivers/video/omap2/displays/panel-s6e8aa0.c

--- drivers/video/omap2/displays/panel-s6e8aa0.c
+++ drivers/video/omap2/displays/panel-s6e8aa0.c
@@ -37,6 +37,9 @@
 #include <linux/i2c.h>
 #include <linux/uaccess.h>
 
+#ifdef CONFIG_COLOR_CONTROL
+#include <linux/color_control.h>
+#endif
 
 #include <video/omapdss.h>
 
@@ -195,6 +198,14 @@ const u8 s6e8aa0_mtp_lock[] = {
 	0xA5,
 	0xA5,
 };
+
+#ifdef CONFIG_COLOR_CONTROL
+struct omap_dss_device * lcd_dev;
+
+struct s6e8aa0_data * s6_data;
+
+int v1_offset[3] = {0, 0, 0};
+#endif
 
 static int s6e8aa0_write_reg(struct omap_dss_device *dssdev, u8 reg, u8 val)
 {
@@ -769,11 +780,16 @@ static void s6e8aa0_setup_gamma_regs(struct s6e8aa0_data *s6, u8 gamma_regs[],
 				__func__, adj, v0, v[V1], c);
 			adj = clamp_t(int, adj, adj_min, adj_max);
 		}
+<<<<<<< HEAD
 #ifdef CONFIG_COLOR_HACK
         int adj_hack = adj + ((hacky_v1_offset[c] * (int)adj) / 100);
         if (adj_hack > adj_max)
             adj_hack = adj_max;
         gamma_regs[gamma_reg_index(c, V1)] = adj_hack;
+=======
+#ifdef CONFIG_COLOR_CONTROL
+		gamma_regs[gamma_reg_index(c, V1)] = min(max(adj +  v1_offset[c], 0), 255);
+>>>>>>> fffe890... Added Color Control version 4.
 #else
 		gamma_regs[gamma_reg_index(c, V1)] = adj;
 #endif
@@ -1134,6 +1150,7 @@ static void s6e8aa0_adjust_brightness_from_mtp(struct s6e8aa0_data *s6)
 	     sizeof(*s6->brightness_table), s6e8aa0_cmp_gamma_entry, NULL);
 }
 
+<<<<<<< HEAD
 
 #ifdef CONFIG_COLOR_HACK
 void ColorGammaUpdate(void)
@@ -1156,6 +1173,23 @@ void brightnessUpdate(void)
         dsi_bus_unlock(lcd_);
     }
 }
+=======
+#ifdef CONFIG_COLOR_CONTROL
+void colorcontrol_update(bool multiplier_updated)
+{
+    if (multiplier_updated)
+	s6e8aa0_adjust_brightness_from_mtp(s6_data);
+
+    if (lcd_dev->state == OMAP_DSS_DISPLAY_ACTIVE) {
+	dsi_bus_lock(lcd_dev);
+	s6e8aa0_update_brightness(lcd_dev);
+	dsi_bus_unlock(lcd_dev);
+    }
+
+    return;
+}
+EXPORT_SYMBOL(colorcontrol_update);
+>>>>>>> fffe890... Added Color Control version 4.
 #endif
 
 static s16 s9_to_s16(s16 v)
@@ -1820,6 +1854,7 @@ static int s6e8aa0_probe(struct omap_dss_device *dssdev)
 	if (cpu_is_omap44xx())
 		s6->force_update = true;
 
+<<<<<<< HEAD
 #ifdef CONFIG_COLOR_HACK
     misc_register(&samoled_color_device);
     if (sysfs_create_group(&samoled_color_device.this_device->kobj, &samoled_color_group) < 0)
@@ -1832,6 +1867,14 @@ static int s6e8aa0_probe(struct omap_dss_device *dssdev)
     original_color_adj_original_mults[0] = s6->pdata->factory_info->color_adj.mult[0];
     original_color_adj_original_mults[1] = s6->pdata->factory_info->color_adj.mult[1];
     original_color_adj_original_mults[2] = s6->pdata->factory_info->color_adj.mult[2];
+=======
+#ifdef CONFIG_COLOR_CONTROL
+	lcd_dev = dssdev;
+	s6_data = s6;
+
+	colorcontrol_register_offset(v1_offset);
+	colorcontrol_register_multiplier(s6->pdata->factory_info->color_adj.mult);
+>>>>>>> fffe890... Added Color Control version 4.
 #endif
 
 	dev_dbg(&dssdev->dev, "s6e8aa0_probe\n");
