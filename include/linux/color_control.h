be46f4084919f2c677407bd06abb09f4a17cfefe
Derek Rodriguez <drod2169@gmail.com>
2012-09-04 13:28:36 -0400

    Revert "This is going to be reverted."
    This reverts commit bd6b7f616e37fa7f3506af436289a1296c3e5c5e.

--- include/linux/color_control.h
+++
@@ -1,9 +0,0 @@
-/* include/linux/color_control.h */
-
-#ifndef _LINUX_COLOR_CONTROL_H
-#define _LINUX_COLOR_CONTROL_H
-
-extern void colorcontrol_register_offset(int * offset);
-extern void colorcontrol_register_multiplier(u32 * multiplier);
-
-#endif
