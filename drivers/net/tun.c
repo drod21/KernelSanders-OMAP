ae717880b4c1901df9fe3b1b8f85f32feb0262e9
Derek Rodriguez <drod2169@gmail.com>
2012-11-04 14:00:58 -0500

    Fix compile

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1245,7 +1245,7 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 	}
 #endif
 
-	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89)
+	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89) {
 		if (copy_from_user(&ifr, argp, ifreq_len))
 			return -EFAULT;
 	} else {
